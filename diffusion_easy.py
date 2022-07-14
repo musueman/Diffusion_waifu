2f3c5f0979eeda0ee7ea3bd2eb9f5623bbf71726
Rubiksman78 <samuelabc78310@gmail.com>
2022-07-14 15:29:23 +0200

    easy-ver

---
+++ diffusion_easy.py
@@ -0,0 +1,27 @@
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
+
+model = Unet(
+    dim = 64,
+    dim_mults = (1, 2, 4, 8)
+).cuda()
+
+diffusion = GaussianDiffusion(
+    model,
+    image_size = 128,
+    timesteps = 1000,           # number of steps
+    sampling_timesteps = 250,   # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+    loss_type = 'l1'            # L1 or L2
+).cuda()
+
+trainer = Trainer(
+    diffusion,
+    '../anime_face',
+    train_batch_size = 32,
+    train_lr = 8e-5,
+    train_num_steps = 700000,         # total training steps
+    gradient_accumulate_every = 2,    # gradient accumulation steps
+    ema_decay = 0.995,                # exponential moving average decay
+    amp = True                        # turn on mixed precision
+)
+
+trainer.train()

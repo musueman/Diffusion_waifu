f7422f1e74dd59ea66525dc85ee9485fbbd40e89
Rubiksman78 <samuelabc78310@gmail.com>
2022-08-08 18:11:53 +0200

    oj

--- diffusion.py
+++ diffusion.py
@@ -141,6 +141,7 @@ def train(epochs):
                 #all_images_list = list(map(lambda n:sample(model,image_size,batch_size=n,channels=channels),batches))
                 all_images_list = sample(model,image_size,batch_size=batch_size,channels=channels)
                 #all_images_list.pop(0)
+                print(all_images_list)
                 all_images = torch.cat(all_images_list,dim=0)
                 all_images = (all_images + 1) * 0.5
                 save_image(all_images,f"{results_folder}/sample-{milestone}.png",nrow=6)
